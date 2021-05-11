fe8389062d02ded70f743e6f8acf787dd146fb23
sergeypospelov <sergeypospelov59@gmail.com>
2021-05-11 17:52:27 +0300

    final classes

--- src/main/scala/scalaBot/calendar/CalendarItem.scala
+++ src/main/scala/scalaBot/calendar/CalendarItem.scala
@@ -7,10 +7,10 @@ sealed trait CalendarItem {
   def getEvents: List[Event]
 }
 
-case class AssignmentItem(description: String) extends CalendarItem {
+final case class AssignmentItem(description: String) extends CalendarItem {
   override def getEvents: List[Event] = ???
 }
 
-case class ClassItem(override val description: String) extends CalendarItem {
+final case class ClassItem(override val description: String) extends CalendarItem {
   override def getEvents: List[Event] = ???
 }
