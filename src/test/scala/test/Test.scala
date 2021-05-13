8c45bc850fb642d36012d09cb02fb74ace58c3f0
sergeypospelov <sergeypospelov59@gmail.com>
2021-05-13 11:10:19 +0300

    fix CE & unused imports

--- src/test/scala/test/Test.scala
+++ src/test/scala/test/Test.scala
@@ -2,7 +2,6 @@ package test
 
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.must.Matchers
-import scalaBot.parser.ParserICS
 
 class Test extends AnyFunSuite with Matchers {
   test("useless") {
